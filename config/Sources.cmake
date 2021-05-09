1e29d9f239eff789476e2e3bee45a7b088dc5436
Artur Wasmut <awasmut@uni-koblenz.de>
2021-05-09 14:09:57 +0200

    [#24] Initial implementation of a core class.

--- config/Sources.cmake
+++ config/Sources.cmake
@@ -1,9 +1,12 @@
 
+# adding all source files and header files of the framework:
 set(vkcv_sources
-		${vkcv_source}/vkcv/Context.hpp
-		${vkcv_source}/vkcv/Context.cpp
-		${vkcv_source}/vkcv/Window.hpp
+		${vkcv_include}/vkcv/Core.hpp
+		${vkcv_source}/vkcv/Core.cpp
+
+		${vkcv_include}/vkcv/Handles.hpp
+		${vkcv_source}/vkcv/Handles.cpp
+
+		${vkcv_include}/vkcv/Window.hpp
 		${vkcv_source}/vkcv/Window.cpp
-		${vkcv_source}/vkcv/CoreManager.hpp
-		${vkcv_source}/vkcv/CoreManager.cpp
 )
---
+++ src/vkcv/Handles.cpp
@@ -0,0 +1,1 @@
+#include "vkcv/Handles.hpp"
