094f2597ad2abf1e96ee9e9d320668383e38c0b3
Tobias Frisch <tfrisch@uni-koblenz.de>
2021-05-11 12:09:05 +0200

    [#24] Fixed sources in cmake
    Signed-off-by: Tobias Frisch <tfrisch@uni-koblenz.de>

--- config/Sources.cmake
+++ config/Sources.cmake
@@ -1,7 +1,7 @@
 
 # adding all source files and header files of the framework:
 set(vkcv_sources
-		${vkcv_source}/vkcv/Context.hpp
+		${vkcv_include}/vkcv/Context.hpp
 		${vkcv_source}/vkcv/Context.cpp
 
 		${vkcv_include}/vkcv/Core.hpp
