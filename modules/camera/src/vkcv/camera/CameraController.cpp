0e7d48c8e78b8c4e68664035fc84cebf9149b844
Josh Morgenstern <josh@morgenstern.dev>
2021-06-15 00:29:14 +0200

    [#42] remove camera and window from controller

--- modules/camera/src/vkcv/camera/CameraController.cpp
+++ modules/camera/src/vkcv/camera/CameraController.cpp
@@ -1,29 +1,17 @@
 #include "vkcv/camera/CameraController.hpp"
+#include <iostream>
 
 namespace vkcv {
-
-    Camera& CameraController::getCamera() {
-        return *m_camera;
+    
+    void CameraController::updateCamera(double deltaTime, Camera &camera) {
     }
 
-    void CameraController::setCamera(Camera &camera) {
-        m_camera = &camera;
-    }
+    void CameraController::keyCallback(int key, int scancode, int action, int mods, Camera &camera) {}
 
-    void CameraController::setWindow(Window &window) {
-        m_window = &window;
-        m_lastX = m_window->getWidth() / 2.0;
-        m_lastY = m_window->getHeight() / 2.0;
-    }
+    void CameraController::scrollCallback( double offsetX, double offsetY, Camera &camera) {}
 
-    void CameraController::updateCamera(double deltaTime) {}
+    void CameraController::mouseMoveCallback(double offsetX, double offsetY, Camera &camera) {}
 
-    void CameraController::keyCallback(int key, int scancode, int action, int mods) {}
-
-    void CameraController::scrollCallback( double offsetX, double offsetY) {}
-
-    void CameraController::mouseMoveCallback(double offsetX, double offsetY) {}
-
-    void CameraController::mouseButtonCallback(int button, int action, int mods) {}
+    void CameraController::mouseButtonCallback(int button, int action, int mods, Camera &camera) {}
 
 }
