1c92affccffe7f7309300c0e4a69239719c964a9
Simeon Hermann <shermann04@uni-koblenz.de>
2021-06-21 13:47:40 +0200

    [#76] try to implement a bitmask for the shader stages

--- include/vkcv/ShaderStage.hpp
+++ include/vkcv/ShaderStage.hpp
@@ -1,15 +1,14 @@
 #pragma once
 
 namespace vkcv {
-	
-	enum class ShaderStage
-	{
-		VERTEX,
-		TESS_CONTROL,
-		TESS_EVAL,
-		GEOMETRY,
-		FRAGMENT,
-		COMPUTE
-	};
 
+	enum class ShaderStage : uint32_t
+	{
+		VERTEX = 0x00000001,
+		TESS_CONTROL = 0x00000002,
+		TESS_EVAL = 0x00000004,
+		GEOMETRY = 0x00000008,
+		FRAGMENT = 0x00000016,
+		COMPUTE = 0x00000032
+	};
 }
