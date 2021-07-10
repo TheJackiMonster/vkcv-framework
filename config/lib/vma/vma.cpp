5b24326279dc24acb361141ec8f4a87fd7355006
Tobias Frisch <tfrisch@uni-koblenz.de>
2021-07-10 13:01:05 +0200

    [#79] Potential fix for MinGW problems with VMA
    Signed-off-by: Tobias Frisch <tfrisch@uni-koblenz.de>

--- config/lib/vma/vma.cpp
+++ config/lib/vma/vma.cpp
@@ -1,6 +1,16 @@
 
+#include <mutex>
+
 #ifndef NDEBUG
 #define _DEBUG
+#endif
+
+#ifndef _WIN32
+
+#ifndef _aligned_malloc
+#define _aligned_malloc(alignment, size) aligned_alloc((alignment), (size))
+#endif
+
 #endif
 
 #define VMA_IMPLEMENTATION
