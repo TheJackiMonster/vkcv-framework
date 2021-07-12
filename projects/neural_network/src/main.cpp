28efc95353adb8b61984e64a548bda75f4ccf517
Leonie Franken <lfranken@uni-koblenz.de>
2021-07-12 14:50:20 +0200

    [#91] ordner, cmake und main aufgesetzt

---
+++ projects/neural_network/src/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vkcv/Core.hpp>
+#include <GLFW/glfw3.h>
+#include <vkcv/camera/CameraManager.hpp>
+#include <chrono>
+#include <vkcv/asset/asset_loader.hpp>
+#include <vkcv/shader/GLSLCompiler.hpp>
+#include <vkcv/scene/Scene.hpp>
+
+int main(int argc, const char** argv) {
+
+
+
+    return 0;
+}
