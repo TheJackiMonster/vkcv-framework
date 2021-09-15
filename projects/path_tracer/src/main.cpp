bfbd4296f5402a74d51475bf559e104aa39c6f19
Alex Laptop <alexander.gauggel@web.de>
2021-09-15 10:00:22 +0200

    Add editable roughness

--- projects/path_tracer/src/main.cpp
+++ projects/path_tracer/src/main.cpp
@@ -10,13 +10,13 @@ int main(int argc, const char** argv) {
 
 	// structs must match shader version
 	struct Material {
-		Material(const glm::vec3& emission, const glm::vec3& albedo, float ks)
-			: emission(emission), albedo(albedo), ks(ks) {}
+		Material(const glm::vec3& emission, const glm::vec3& albedo, float ks, float roughness)
+			: emission(emission), albedo(albedo), ks(ks), roughness(roughness){}
 
 		glm::vec3   emission;
 		float       ks;
 		glm::vec3   albedo;
-		float       padding1;
+		float       roughness;
 	};
 
 	struct Sphere {
@@ -147,11 +147,11 @@ int main(int argc, const char** argv) {
 	typedef std::pair<std::string, Material> MaterialSetting;
 
 	std::vector<MaterialSetting> materialSettings;
-	materialSettings.emplace_back(MaterialSetting("white",  Material(glm::vec3(0),    glm::vec3(0.65),            0)));
-	materialSettings.emplace_back(MaterialSetting("red",    Material(glm::vec3(0),    glm::vec3(0.5, 0.0, 0.0),   0)));
-	materialSettings.emplace_back(MaterialSetting("green",  Material(glm::vec3(0),    glm::vec3(0.0, 0.5, 0.0),   0)));
-	materialSettings.emplace_back(MaterialSetting("light",  Material(glm::vec3(20),   glm::vec3(0),               0)));
-	materialSettings.emplace_back(MaterialSetting("sphere", Material(glm::vec3(0),    glm::vec3(0.65),            1)));
+	materialSettings.emplace_back(MaterialSetting("white",  Material(glm::vec3(0),    glm::vec3(0.65),            0, 0.25)));
+	materialSettings.emplace_back(MaterialSetting("red",    Material(glm::vec3(0),    glm::vec3(0.5, 0.0, 0.0),   0, 0.25)));
+	materialSettings.emplace_back(MaterialSetting("green",  Material(glm::vec3(0),    glm::vec3(0.0, 0.5, 0.0),   0, 0.25)));
+	materialSettings.emplace_back(MaterialSetting("light",  Material(glm::vec3(20),   glm::vec3(0),               0, 0.25)));
+	materialSettings.emplace_back(MaterialSetting("sphere", Material(glm::vec3(0),    glm::vec3(0.65),            1, 0.25)));
 
 	const uint32_t whiteMaterialIndex   = 0;
 	const uint32_t redMaterialIndex     = 1;
@@ -426,6 +426,7 @@ int main(int argc, const char** argv) {
 
 						updateMaterials |= ImGui::ColorEdit3("Albedo color",    &setting.second.albedo.x);
 						updateMaterials |= ImGui::DragFloat("ks",               &setting.second.ks, 0.01, 0, 1);
+						updateMaterials |= ImGui::DragFloat("roughness",        &setting.second.roughness, 0.01, 0, 1);
 
 					}
 				}
