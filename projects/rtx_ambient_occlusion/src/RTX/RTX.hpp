1fc7bb7d6541f450b5ac7abb8547fa49a2412a30
Vanessa Karolek <vaka1997@uni-koblenz.de>
2021-09-20 15:22:39 +0200

    [#92] add missing subfolder from last commit... because .gitignore

---
+++ projects/rtx_ambient_occlusion/src/RTX/RTX.hpp
@@ -0,0 +1,85 @@
+#pragma once
+
+#include <vector>
+#include "vulkan/vulkan.hpp"
+#include <vkcv/Core.hpp>
+#include "ASManager.hpp"
+
+namespace vkcv::rtx {
+
+    class RTXModule {
+    private:
+
+        Core* m_core;
+        ASManager* m_asManager;
+        vk::Pipeline m_pipeline;
+        vk::PipelineLayout m_pipelineLayout;
+        RTXBuffer m_shaderBindingTableBuffer;
+        vk::DeviceSize m_shaderGroupBaseAlignment;
+
+    public:
+
+        /**
+         * @brief Initializes the @#RTXModule with scene data.
+         * @param core The reference to the @#Core.
+         * @param asManager The reference to the @#ASManager.
+         * @param vertices The vertex data of the scene.
+         * @param indices The index data of the scene.
+         * @param descriptorSetHandles The descriptor set handles for RTX.
+         */
+        RTXModule(Core* core, ASManager* asManager, std::vector<float>& vertices,
+            std::vector<uint32_t>& indices, std::vector<vkcv::DescriptorSetHandle>& descriptorSetHandles);
+
+        /**
+         * @brief Default #RTXModule destructor.
+         */
+        ~RTXModule();
+
+        /**
+         * @brief Returns the RTX pipeline.
+         * @return The RTX pipeline.
+         */
+        vk::Pipeline getPipeline();
+
+        /**
+         * @brief Returns the shader binding table buffer.
+         * @return The shader binding table buffer.
+         */
+        vk::Buffer getShaderBindingTableBuffer();
+
+        /**
+         * @brief Returns the shader group base alignment for partitioning the shader binding table buffer.
+         * @return The shader group base alignment.
+         */
+        vk::DeviceSize getShaderGroupBaseAlignment();
+
+        /**
+         * @brief Returns the RTX pipeline layout.
+         * @return The RTX pipeline layout.
+         */
+        vk::PipelineLayout getPipelineLayout();
+
+        /**
+         * @brief Sets the shader group base alignment and creates the shader binding table by allocating a shader
+         * binding table buffer. The allocation depends on @p shaderCount and the shader group base alignment.
+         * @param shaderCount The amount of shaders to be used for RTX.
+         */
+        void createShaderBindingTable(uint32_t shaderCount);
+
+        /**
+         * @brief Creates Descriptor-Writes for RTX
+         * @param descriptorSetHandles The descriptorSetHandles for RTX.
+         */
+        void RTXDescriptors(std::vector<vkcv::DescriptorSetHandle>& descriptorSetHandles);
+
+        /**
+         * @brief Creates the RTX pipeline and the RTX pipeline layout. Currently, only RayGen, RayClosestHit and
+         * RayMiss are supported.
+         * @param pushConstantSize The size of the push constant used in the RTX shaders.
+         * @param descriptorSetLayouts The descriptor set layout handles.
+         * @param rtxShader The RTX shader program.
+         */
+        void createRTXPipelineAndLayout(uint32_t pushConstantSize, std::vector<DescriptorSetLayoutHandle> descriptorSetLayouts, ShaderProgram &rtxShader);
+    };
+
+}
---
+++ projects/rtx_ambient_occlusion/src/RTX/RTXExtensions.cpp
@@ -0,0 +1,130 @@
+#include "RTXExtensions.hpp"
+
+namespace vkcv::rtx{
+
+RTXExtensions::RTXExtensions()
+{
+
+    // prepare needed raytracing extensions
+    m_instanceExtensions = {
+            VK_KHR_GET_PHYSICAL_DEVICE_PROPERTIES_2_EXTENSION_NAME
+    };
+    m_deviceExtensions = {
+            VK_KHR_MAINTENANCE3_EXTENSION_NAME,
+            VK_EXT_DESCRIPTOR_INDEXING_EXTENSION_NAME,
+            VK_KHR_BUFFER_DEVICE_ADDRESS_EXTENSION_NAME,
+            VK_KHR_DEFERRED_HOST_OPERATIONS_EXTENSION_NAME,
+            VK_KHR_ACCELERATION_STRUCTURE_EXTENSION_NAME,
+            VK_KHR_SPIRV_1_4_EXTENSION_NAME,
+            VK_KHR_RAY_TRACING_PIPELINE_EXTENSION_NAME,
+            VK_KHR_RAY_QUERY_EXTENSION_NAME,
+            VK_KHR_PIPELINE_LIBRARY_EXTENSION_NAME
+    };
+
+    // get all features required by the device extensions
+    for (auto deviceExtension : m_deviceExtensions) {
+        m_features.requireExtension(deviceExtension);
+    }
+
+    /* FIXME: We must disable features that will be mentioned as "not supported" by the FeatureManager. If every unsupported feature is disabled, this should work.
+     * Maybe we find a better workaround...
+     */
+    m_features.requireFeature<vk::PhysicalDeviceVulkan12Features>(
+        [](vk::PhysicalDeviceVulkan12Features& features) {
+            features.setSamplerMirrorClampToEdge(true);
+            features.setDrawIndirectCount(true);
+            features.setStorageBuffer8BitAccess(true);
+            features.setUniformAndStorageBuffer8BitAccess(true);
+            features.setStoragePushConstant8(true);
+            features.setShaderBufferInt64Atomics(true);
+            features.setShaderSharedInt64Atomics(true);
+            features.setShaderFloat16(true);
+            features.setShaderInt8(true);
+            features.setDescriptorIndexing(true);
+            features.setShaderInputAttachmentArrayDynamicIndexing(true);
+            features.setShaderUniformTexelBufferArrayDynamicIndexing(true);
+            features.setShaderStorageTexelBufferArrayDynamicIndexing(true);
+            features.setShaderUniformBufferArrayNonUniformIndexing(true);
+            features.setShaderSampledImageArrayNonUniformIndexing(true);
+            features.setShaderStorageBufferArrayNonUniformIndexing(true);
+            features.setShaderStorageImageArrayNonUniformIndexing(true);
+            features.setShaderInputAttachmentArrayNonUniformIndexing(true);
+            features.setShaderUniformTexelBufferArrayNonUniformIndexing(true);
+            features.setShaderStorageTexelBufferArrayNonUniformIndexing(true);
+            features.setDescriptorBindingUniformBufferUpdateAfterBind(true);
+            features.setDescriptorBindingSampledImageUpdateAfterBind(true);
+            features.setDescriptorBindingStorageImageUpdateAfterBind(true);
+            features.setDescriptorBindingStorageBufferUpdateAfterBind(true);
+            features.setDescriptorBindingUniformTexelBufferUpdateAfterBind(true);
+            features.setDescriptorBindingStorageTexelBufferUpdateAfterBind(true);
+            features.setDescriptorBindingUpdateUnusedWhilePending(true);
+            features.setDescriptorBindingPartiallyBound(true);
+            features.setDescriptorBindingVariableDescriptorCount(true);
+            features.setRuntimeDescriptorArray(true);
+            features.setSamplerFilterMinmax(true);
+            features.setScalarBlockLayout(true);
+            features.setImagelessFramebuffer(true);
+            features.setUniformBufferStandardLayout(true);
+            features.setShaderSubgroupExtendedTypes(true);
+            features.setSeparateDepthStencilLayouts(true);
+            features.setHostQueryReset(true);
+            features.setTimelineSemaphore(true);
+            features.setBufferDeviceAddress(true);
+            features.setBufferDeviceAddressCaptureReplay(true);
+            features.setBufferDeviceAddressMultiDevice(true);
+            features.setVulkanMemoryModel(true);
+            features.setVulkanMemoryModelDeviceScope(true);
+            features.setVulkanMemoryModelAvailabilityVisibilityChains(true);
+            features.setShaderOutputViewportIndex(true);
+            features.setShaderOutputLayer(true);
+            features.setSubgroupBroadcastDynamicId(true);
+        });
+    m_features.requireFeature<vk::PhysicalDeviceVulkan11Features>(
+        [](vk::PhysicalDeviceVulkan11Features& features) {
+            features.setMultiview(true);
+            features.setMultiviewGeometryShader(true);
+            features.setMultiviewTessellationShader(true);
+            //                    features.setProtectedMemory(true);    // not supported
+            features.setSamplerYcbcrConversion(true);
+            features.setShaderDrawParameters(true);
+            features.setStorageBuffer16BitAccess(true);
+            //                    features.setStorageInputOutput16(true);   // not supported
+            features.setStoragePushConstant16(true);
+            features.setUniformAndStorageBuffer16BitAccess(true);
+            features.setVariablePointers(true);
+            features.setVariablePointersStorageBuffer(true);
+        });
+    m_features.requireFeature<vk::PhysicalDeviceAccelerationStructureFeaturesKHR>(
+        [](vk::PhysicalDeviceAccelerationStructureFeaturesKHR& features) {
+            features.setAccelerationStructure(true);
+            features.setAccelerationStructureCaptureReplay(true);
+            //                    features.setAccelerationStructureIndirectBuild(true); // not supported
+            //                    features.setAccelerationStructureHostCommands(true);  // not supported
+            features.setDescriptorBindingAccelerationStructureUpdateAfterBind(true);
+        });
+    m_features.requireExtensionFeature<vk::PhysicalDeviceRayTracingPipelineFeaturesKHR>(
+        VK_KHR_RAY_TRACING_PIPELINE_EXTENSION_NAME, [](vk::PhysicalDeviceRayTracingPipelineFeaturesKHR& features) {
+            features.setRayTracingPipeline(true);
+            //                    features.setRayTracingPipelineShaderGroupHandleCaptureReplay(true);   // not supported
+            //                    features.setRayTracingPipelineShaderGroupHandleCaptureReplayMixed(true);  // not supported
+            features.setRayTracingPipelineTraceRaysIndirect(true);
+            features.setRayTraversalPrimitiveCulling(true);
+        });
+}
+
+std::vector<const char*> RTXExtensions::getInstanceExtensions()
+{
+	return m_instanceExtensions;
+}
+
+std::vector<const char*> RTXExtensions::getDeviceExtensions()
+{
+	return m_deviceExtensions;
+}
+
+vkcv::Features RTXExtensions::getFeatures()
+{
+	return m_features;
+}
+
+}
