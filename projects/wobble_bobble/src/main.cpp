ed3cff988451686d163bb7d19b5329a8bc436401
Tobias Frisch <tfrisch@uni-koblenz.de>
2022-01-31 03:00:36 +0100

    Corrected error with unsigned loop indices
    Signed-off-by: Tobias Frisch <tfrisch@uni-koblenz.de>

--- projects/wobble_bobble/src/main.cpp
+++ projects/wobble_bobble/src/main.cpp
@@ -40,7 +40,7 @@ void distributeParticles(Particle *particles, size_t count, const glm::vec3& cen
 		
 		particles[i].position = center + offset;
 		particles[i].size = size;
-		particles[i].velocity = glm::vec3(0.0f, 0.1f, 0.0f);
+		particles[i].velocity = glm::vec3(0.0f);
 		
 		volume += size;
 	}
