593a404ad3927c45220d1317e8c30dafcb78030e
Tobias Frisch <tfrisch@uni-koblenz.de>
2022-05-22 23:58:38 +0200

    Added doxygen comments to sync resources
    Signed-off-by: Tobias Frisch <tfrisch@uni-koblenz.de>

--- include/vkcv/SyncResources.hpp
+++ include/vkcv/SyncResources.hpp
@@ -15,9 +15,41 @@ namespace vkcv {
 		vk::Fence		presentFinished;
 	};
 
-	SyncResources	createSyncResources(const vk::Device &device);
-	void			destroySyncResources(const vk::Device &device, const SyncResources &resources);
-	vk::Fence		createFence(const vk::Device &device);
-	void			waitForFence(const vk::Device& device, const vk::Fence& fence);
+	/**
+	 * @brief Creates new synchronization resources for drawing
+	 * and presenting with a swapchain.
+	 *
+	 * @param[in,out] device Vulkan-Device
+	 * @return New created synchronization resources
+	 */
+	SyncResources createSyncResources(const vk::Device &device);
+	
+	/**
+	 * @brief Destroys the synchronization resources with a
+	 * given device.
+	 *
+	 * @param[in,out] device Vulkan-Device
+	 * @param[in,out] resources Synchronizazion resources
+	 */
+	void destroySyncResources(const vk::Device &device,
+							  const SyncResources &resources);
+	
+	/**
+	 * @brief Creates a new fence with a given device and
+	 * returns it.
+	 *
+	 * @param[in,out] device Vulkan-Device
+	 * @return New created fence
+	 */
+	vk::Fence createFence(const vk::Device &device);
+	
+	/**
+	 * @brief Calls a given device to wait for a specific fence.
+	 *
+	 * @param[in,out] device Vulkan-Device
+	 * @param[in] fence Vulkan-Fence
+	 */
+	void waitForFence(const vk::Device& device,
+					  const vk::Fence& fence);
 	
 }
