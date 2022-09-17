c7ab753d86ff1117138cf09bc816ae11f3307fd1
Tobias Frisch <tfrisch@uni-koblenz.de>
2022-09-17 22:42:39 +0200

    Fix parameter type completed in Pass.hpp
    Signed-off-by: Tobias Frisch <tfrisch@uni-koblenz.de>

--- include/vkcv/Pass.hpp
+++ include/vkcv/Pass.hpp
@@ -18,7 +18,7 @@ namespace vkcv {
 						  Multisampling multisampling = Multisampling::None);
 
 	PassHandle passSwapchain(Core &core, const SwapchainHandle &swapchain,
-							 const std::vector<vk::Format> formats, bool clear = true,
+							 const std::vector<vk::Format> &formats, bool clear = true,
 							 Multisampling multisampling = Multisampling::None);
 
 } // namespace vkcv
