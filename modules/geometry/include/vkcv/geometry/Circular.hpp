ff2323f36e46d745b6de7e5eddf6b4cf3bbd78a1
Tobias Frisch <tfrisch@uni-koblenz.de>
2022-10-07 17:44:51 +0200

    Add doxygen comments to geometry module
    Signed-off-by: Tobias Frisch <tfrisch@uni-koblenz.de>

--- modules/geometry/include/vkcv/geometry/Circular.hpp
+++ modules/geometry/include/vkcv/geometry/Circular.hpp
@@ -9,30 +9,97 @@ namespace vkcv::geometry {
      * @{
      */
 	
+	/**
+	 * A basic class to provide attributes for circular geometry.
+	 */
 	class Circular {
 	private:
+		/**
+		 * Radius of the circular part of the geometry.
+		 */
 		float m_radius;
+		
+		/**
+		 * Resolution in case of generating the geometry in a
+		 * discrete way.
+		 */
 		size_t m_resolution;
 	
 	public:
+		/**
+		 * Constructor creating circular geometry by a given
+		 * radius and a resolution also provides a default.
+		 *
+		 * @param[in] radius Radius of the circular geometry
+		 * @param[in] resoltion Resolution of the circular geometry
+		 */
 		explicit Circular(float radius, size_t resoltion = 10);
 		
+		/**
+         * Copy-constructor of a circular geometry.
+         *
+         * @param[in] other Other circular geometry
+         */
 		Circular(const Circular& other) = default;
+		
+		/**
+         * Move-constructor of a circular geometry.
+         *
+         * @param[in] other Other circular geometry
+         */
 		Circular(Circular&& other) = default;
 		
+		/**
+         * Destructor of a circular geometry.
+         */
 		~Circular() = default;
 		
+		/**
+         * Copy-operator of a circular geometry.
+         *
+         * @param[in] other Other circular geometry
+         * @return Reference to this circular geometry
+         */
 		Circular& operator=(const Circular& other) = default;
+		
+		/**
+         * Move-operator of a circular geometry.
+         *
+         * @param[in] other Other circular geometry
+         * @return Reference to this circular geometry
+         */
 		Circular& operator=(Circular&& other) = default;
 		
+		/**
+		 * Return the radius of the circular part of the geometry.
+		 *
+		 * @return Radius of the circular geometry
+		 */
 		[[nodiscard]]
 		float getRadius() const;
 		
+		/**
+		 * Set the radius of the circular part of the geometry.
+		 *
+		 * @param[in] radius Radius of the circular geometry
+		 */
 		void setRadius(float radius);
 		
+		/**
+		 * Return the resolution of the geometry for discrete
+		 * generation.
+		 *
+		 * @return Resolution of the circular geometry
+		 */
 		[[nodiscard]]
 		size_t getResolution() const;
 		
+		/**
+		 * Set the resolution of the geometry for any discrete
+		 * generation.
+		 *
+		 * @param[in] resolution Resolution of the circular geometry
+		 */
 		void setResolution(size_t resolution);
 		
 	};
--- modules/geometry/include/vkcv/geometry/Sphere.hpp
+++ modules/geometry/include/vkcv/geometry/Sphere.hpp
@@ -10,24 +10,79 @@ namespace vkcv::geometry {
      * @{
      */
 	
+	/**
+	 * A class to use geometry in form of a sphere.
+	 */
 	class Sphere : public Volume, public Circular {
-	private:
-		float m_radius;
-		
 	public:
+		/**
+		 * Constructor creating sphere by a given position
+		 * as 3D vector and a radius.
+		 *
+		 * @param[in] position Position of the sphere as 3D vector
+		 * @param[in] radius Radius of the sphere
+		 */
 		Sphere(const glm::vec3& position, float radius);
-	
+		
+		/**
+         * Copy-constructor of a sphere.
+         *
+         * @param[in] other Other sphere
+         */
 		Sphere(const Sphere& other) = default;
+		
+		/**
+         * Move-constructor of a sphere.
+         *
+         * @param[in] other Other sphere
+         */
 		Sphere(Sphere&& other) = default;
-	
+		
+		/**
+         * Destructor of a sphere.
+         */
 		~Sphere() = default;
 		
+		/**
+         * Copy-operator of a sphere.
+         *
+         * @param[in] other Other sphere
+         * @return Reference to this sphere
+         */
 		Sphere& operator=(const Sphere& other) = default;
+		
+		/**
+         * Move-operator of a sphere.
+         *
+         * @param[in] other Other sphere
+         * @return Reference to this sphere
+         */
 		Sphere& operator=(Sphere&& other) = default;
 		
+		/**
+		 * Returns the signed distance from a point to the closest
+		 * surface of the sphere.
+		 *
+		 * The result is negative if the point is contained by the
+		 * sphere.
+		 *
+		 * @param[in] point Point as 3D vector
+		 * @return Signed distance from point to surface
+		 */
 		[[nodiscard]]
 		float distanceTo(const glm::vec3& point) override;
 		
+		/**
+		 * Generates a vertex data structure, which can be
+		 * used for rendering via draw calls, containing
+		 * the sphere in a discrete form.
+		 *
+		 * The vertex data will store positions, normals and
+		 * UV-coordinates as vertex attributes.
+		 *
+		 * @param[in,out] core Core instance
+		 * @return Vertex data with generated geometry
+		 */
 		[[nodiscard]]
 		VertexData generateVertexData(Core& core) const override;
 		
