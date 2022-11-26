a8d661b28c7b0baa1ec9ec28b71bdeaffbe817d2
Tobias Frisch <tfrisch@uni-koblenz.de>
2022-11-26 13:12:10 +0100

    Update Vulkan and VMA headers, fix issue in RTX project and adjust shader stage flags
    Signed-off-by: Tobias Frisch <tfrisch@uni-koblenz.de>

--- include/vkcv/ShaderStage.hpp
+++ include/vkcv/ShaderStage.hpp
@@ -14,48 +14,51 @@ namespace vkcv {
 	 * @brief Enum class to specify the stage of a shader.
 	 */
 	enum class ShaderStage : VkShaderStageFlags {
-		VERTEX = static_cast<VkShaderStageFlags>(vk::ShaderStageFlagBits::eVertex),
+		VERTEX = static_cast<VkShaderStageFlags>(VULKAN_HPP_NAMESPACE::ShaderStageFlagBits::eVertex),
 		TESS_CONTROL =
-			static_cast<VkShaderStageFlags>(vk::ShaderStageFlagBits::eTessellationControl),
+			static_cast<VkShaderStageFlags>(VULKAN_HPP_NAMESPACE::ShaderStageFlagBits::eTessellationControl),
 		TESS_EVAL =
-			static_cast<VkShaderStageFlags>(vk::ShaderStageFlagBits::eTessellationEvaluation),
-		GEOMETRY = static_cast<VkShaderStageFlags>(vk::ShaderStageFlagBits::eGeometry),
-		FRAGMENT = static_cast<VkShaderStageFlags>(vk::ShaderStageFlagBits::eFragment),
-		COMPUTE = static_cast<VkShaderStageFlags>(vk::ShaderStageFlagBits::eCompute),
-		TASK = static_cast<VkShaderStageFlags>(vk::ShaderStageFlagBits::eTaskNV),
-		MESH = static_cast<VkShaderStageFlags>(vk::ShaderStageFlagBits::eMeshNV),
-		RAY_GEN = static_cast<VkShaderStageFlags>(vk::ShaderStageFlagBits::eRaygenKHR),
-		RAY_ANY_HIT = static_cast<VkShaderStageFlags>(vk::ShaderStageFlagBits::eAnyHitKHR),
-		RAY_CLOSEST_HIT = static_cast<VkShaderStageFlags>(vk::ShaderStageFlagBits::eClosestHitKHR),
-		RAY_MISS = static_cast<VkShaderStageFlags>(vk::ShaderStageFlagBits::eMissKHR),
+			static_cast<VkShaderStageFlags>(VULKAN_HPP_NAMESPACE::ShaderStageFlagBits::eTessellationEvaluation),
+		GEOMETRY = static_cast<VkShaderStageFlags>(VULKAN_HPP_NAMESPACE::ShaderStageFlagBits::eGeometry),
+		FRAGMENT = static_cast<VkShaderStageFlags>(VULKAN_HPP_NAMESPACE::ShaderStageFlagBits::eFragment),
+		COMPUTE = static_cast<VkShaderStageFlags>(VULKAN_HPP_NAMESPACE::ShaderStageFlagBits::eCompute),
+		TASK = static_cast<VkShaderStageFlags>(VULKAN_HPP_NAMESPACE::ShaderStageFlagBits::eTaskNV),
+		MESH = static_cast<VkShaderStageFlags>(VULKAN_HPP_NAMESPACE::ShaderStageFlagBits::eMeshNV),
+		RAY_GEN = static_cast<VkShaderStageFlags>(VULKAN_HPP_NAMESPACE::ShaderStageFlagBits::eRaygenKHR),
+		RAY_ANY_HIT = static_cast<VkShaderStageFlags>(VULKAN_HPP_NAMESPACE::ShaderStageFlagBits::eAnyHitKHR),
+		RAY_CLOSEST_HIT = static_cast<VkShaderStageFlags>(VULKAN_HPP_NAMESPACE::ShaderStageFlagBits::eClosestHitKHR),
+		RAY_MISS = static_cast<VkShaderStageFlags>(VULKAN_HPP_NAMESPACE::ShaderStageFlagBits::eMissKHR),
 		RAY_INTERSECTION =
-			static_cast<VkShaderStageFlags>(vk::ShaderStageFlagBits::eIntersectionKHR),
-		RAY_CALLABLE = static_cast<VkShaderStageFlags>(vk::ShaderStageFlagBits::eCallableKHR)
+			static_cast<VkShaderStageFlags>(VULKAN_HPP_NAMESPACE::ShaderStageFlagBits::eIntersectionKHR),
+		RAY_CALLABLE = static_cast<VkShaderStageFlags>(VULKAN_HPP_NAMESPACE::ShaderStageFlagBits::eCallableKHR)
 	};
 
-	using ShaderStages = vk::Flags<ShaderStage>;
+	using ShaderStages = VULKAN_HPP_NAMESPACE::Flags<ShaderStage>;
 
 } // namespace vkcv
 
 /**
  * @cond VULKAN_TYPES
  */
-namespace vk {
+namespace VULKAN_HPP_NAMESPACE {
 
 	template <>
 	struct [[maybe_unused]] FlagTraits<vkcv::ShaderStage> {
-		enum : VkFlags {
-			allFlags [[maybe_unused]] =
-				(VkFlags(vkcv::ShaderStage::VERTEX) | VkFlags(vkcv::ShaderStage::TESS_CONTROL)
-				 | VkFlags(vkcv::ShaderStage::TESS_EVAL) | VkFlags(vkcv::ShaderStage::GEOMETRY)
-				 | VkFlags(vkcv::ShaderStage::FRAGMENT) | VkFlags(vkcv::ShaderStage::COMPUTE)
-				 | VkFlags(vkcv::ShaderStage::TASK) | VkFlags(vkcv::ShaderStage::MESH)
-				 | VkFlags(vkcv::ShaderStage::RAY_GEN) | VkFlags(vkcv::ShaderStage::RAY_ANY_HIT)
-				 | VkFlags(vkcv::ShaderStage::RAY_CLOSEST_HIT)
-				 | VkFlags(vkcv::ShaderStage::RAY_MISS)
-				 | VkFlags(vkcv::ShaderStage::RAY_INTERSECTION)
-				 | VkFlags(vkcv::ShaderStage::RAY_CALLABLE))
-		};
+		static VULKAN_HPP_CONST_OR_CONSTEXPR bool isBitmask = true;
+		static VULKAN_HPP_CONST_OR_CONSTEXPR Flags<vkcv::ShaderStage> allFlags =
+				vkcv::ShaderStage::VERTEX
+				| vkcv::ShaderStage::TESS_CONTROL
+				| vkcv::ShaderStage::TESS_EVAL
+				| vkcv::ShaderStage::GEOMETRY
+				| vkcv::ShaderStage::FRAGMENT
+				| vkcv::ShaderStage::TASK
+				| vkcv::ShaderStage::MESH
+				| vkcv::ShaderStage::RAY_GEN
+				| vkcv::ShaderStage::RAY_ANY_HIT
+				| vkcv::ShaderStage::RAY_CLOSEST_HIT
+				| vkcv::ShaderStage::RAY_MISS
+				| vkcv::ShaderStage::RAY_INTERSECTION
+				| vkcv::ShaderStage::RAY_CALLABLE;
 	};
 
 } // namespace vk
@@ -65,8 +68,8 @@ namespace vk {
 
 namespace vkcv {
 
-	constexpr vk::ShaderStageFlags getShaderStageFlags(ShaderStages shaderStages) noexcept {
-		return vk::ShaderStageFlags(static_cast<VkShaderStageFlags>(shaderStages));
+	constexpr VULKAN_HPP_NAMESPACE::ShaderStageFlags getShaderStageFlags(ShaderStages shaderStages) noexcept {
+		return VULKAN_HPP_NAMESPACE::ShaderStageFlags(static_cast<VkShaderStageFlags>(shaderStages));
 	}
 
 	constexpr ShaderStages operator|(ShaderStage stage0, ShaderStage stage1) noexcept {
