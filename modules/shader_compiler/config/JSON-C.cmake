1e775d02255aa022d13b60d741d7ff95eff21862
Jacki <jacki@thejackimonster.de>
2024-10-13 07:01:47 +0200

    Add json-c build directory to includes
    Signed-off-by: Jacki <jacki@thejackimonster.de>

--- modules/shader_compiler/config/JSON-C.cmake
+++ modules/shader_compiler/config/JSON-C.cmake
@@ -4,7 +4,7 @@ use_git_submodule("${vkcv_shader_compiler_lib_path}/json-c" json_c_status)
 if (${json_c_status})
 	add_subdirectory(${vkcv_shader_compiler_lib}/json-c)
 
-	set(JSON_C_INCLUDE_DIR ${vkcv_shader_compiler_lib_path})
+	set(JSON_C_INCLUDE_DIRS ${vkcv_shader_compiler_lib_path} ${CMAKE_CURRENT_BINARY_DIR}/${vkcv_shader_compiler_lib})
 	
 	list(APPEND vkcv_shader_compiler_libraries json-c)
 	list(APPEND vkcv_shader_compiler_includes ${vkcv_shader_compiler_lib})
